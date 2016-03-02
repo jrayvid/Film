74a2bcdf51b6468a31fadf62475e225b8f491090
jon <jrayvid@gmail.com>
2016-03-02 13:28:18 -0500

    added more styling and validations to users model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,13 @@ class User < ActiveRecord::Base
 	has_many :videos
 	has_many :bands
 
+	validates_presence_of :fname, :lname, :city, :state, :email, :username
+	validates_presence_of :password, on: :create 
+
 
 has_attached_file :avatar, :styles => {:large => "500x500>", :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png" 
 validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
+
 
 
 end
